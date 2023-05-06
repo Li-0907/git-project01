4039515aa6f7ae87811a8c1e40ab0100a477edf5
杳然 <2870461090@qq.com>
2023-05-06 09:10:25 +0800

    second commit

--- src/main/java/com/lixingyu/ssm/App.java
+++ src/main/java/com/lixingyu/ssm/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello World!");
         System.out.println("add");
+        System.out.println("This is hotFix");
     }
 
 }
